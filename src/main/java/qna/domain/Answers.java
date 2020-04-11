8fc8203c11d88890728f89d01daec836c9a3c867
kesol0328 <kesol0328@gmail.com>
2020-04-11 15:50:10 +0900

    feat (Answers) : List<Answer>를 가지는 클래스 생성

---
+++ src/main/java/qna/domain/Answers.java
@@ -0,0 +1,11 @@
+package qna.domain;
+
+import java.util.List;
+
+public class Answers {
+    private List<Answer> answers;
+
+    public Answers(List<Answer> answers) {
+        this.answers = answers;
+    }
+}
