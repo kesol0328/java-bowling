e212b8cd16f320b7aa2213e1e1dad98d5103453e
kesol0328 <kesol0328@gmail.com>
2020-04-18 17:25:24 +0900

    fix (Answers) : Answers new ArrayList 로 생성

--- src/main/java/qna/domain/Answers.java
+++ src/main/java/qna/domain/Answers.java
@@ -2,13 +2,14 @@ package qna.domain;
 
 import qna.CannotDeleteException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Answers {
     private List<Answer> answers;
 
     public Answers(List<Answer> answers) {
-        this.answers = answers;
+        this.answers = new ArrayList<>(answers);
     }
 
     public Answers() {
