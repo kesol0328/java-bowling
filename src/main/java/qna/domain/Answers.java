02e31b662bd75689ae6da66f7b4de64cfa141d0e
kesol0328 <kesol0328@gmail.com>
2020-04-18 17:22:19 +0900

    fix (Answers) : Answers 생성 수정

--- src/main/java/qna/domain/Answers.java
+++ src/main/java/qna/domain/Answers.java
@@ -2,14 +2,17 @@ package qna.domain;
 
 import qna.CannotDeleteException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Answers {
     private List<Answer> answers;
 
     public Answers(List<Answer> answers) {
-        this.answers = new ArrayList<>(answers);
+        this.answers = answers;
+    }
+
+    public Answers() {
+
     }
 
     public void checkDeletable(User loginUser) throws CannotDeleteException {
@@ -18,5 +21,9 @@ public class Answers {
                 throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
             }
         }
+    }
+
+    public void addAnswer(Answer answer) {
+        answers.add(answer);
     }
 }
